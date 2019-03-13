b3fabcb6e375b1fdcb75b3d1273f5877cd4fcd6b
anggi nuran <angginur0142@gmail.com>
2019-03-13 16:35:10 +0700

    ubahan

--- aplikasi-pendaftaran/src/main/java/com/projek/prakerin/pendaftaran/entity/Siswa.java
+++ aplikasi-pendaftaran/src/main/java/com/projek/prakerin/pendaftaran/entity/Siswa.java
@@ -1,18 +1,38 @@
 package com.projek.prakerin.pendaftaran.entity;
 
+import java.util.Date;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 @Entity
 public class Siswa {
     
     @Id
-    private String id;    
-    private String nama;      
-    private String alamat;        
-    private String jenisKelamin;     
-    private String agama;       
-    private String asalSekolah;      
+    private String id; 
+    
+    @Column(nullable = false, unique = true)
+    private int nis;   
+    
+    @Column(nullable = false)
+    private String nama;    
+    
+    @Column(nullable = false)
+    private String alamat;   
+    
+    @Column(name = "tanggal_lahir", nullable = false)
+    @Temporal(TemporalType.DATE)
+    private Date tanggalLahir;
+    
+    @Column(nullable = false)
+    private String jenisKelamin; 
+    
+    @Column(nullable = false)
+    private String asalSekolah;  
+
+    @Column(nullable = false)    
     private int nilai;
     
     
