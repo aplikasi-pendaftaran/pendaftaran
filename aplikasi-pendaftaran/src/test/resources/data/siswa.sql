66a58f6137d2c185f495e79cc46741e0c7f28fbe
alyaputerip29 <alyaputerip29@gmail.com>
2019-03-19 01:03:44 +0700

    query siswa

--- aplikasi-pendaftaran/src/test/resources/data/siswa.sql
+++ aplikasi-pendaftaran/src/test/resources/data/siswa.sql
@@ -3,3 +3,6 @@ value ("sw001", 000001, "alya", "bekasi", "2002-06-29", "perempuan", "oxford", 4
 
 insert into siswa (id, nis, nama, alamat, tanggal_lahir, jenis_kelamin, asal_sekolah, nilai)
 value ("sw002", 000002, "anggi", "bekasi", "2002-07-30", "perempuan", "dimanaaja", 50);
+
+insert into siswa (id, nis, nama, alamat, tanggal_lahir, jenis_kelamin, asal_sekolah, nilai)
+value ("sw002", 100002, "anggi", "bekasi", "2002-07-30", "perempuan", "dimanaaja", 50);
