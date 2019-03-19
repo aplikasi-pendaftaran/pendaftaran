f78be24f08e0c72ff90244be2ba4f8043ce28046
anggi nuran <angginur0142@gmail.com>
2019-03-19 10:33:27 +0700

    getter

--- aplikasi-pendaftaran/src/main/java/com/projek/prakerin/pendaftaran/entity/Siswa.java
+++ aplikasi-pendaftaran/src/main/java/com/projek/prakerin/pendaftaran/entity/Siswa.java
@@ -1,18 +1,40 @@
 package com.projek.prakerin.pendaftaran.entity;
 
+import java.util.Date;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import org.hibernate.annotations.GenericGenerator;
 
 @Entity
 public class Siswa {
-    @Id
+    @Id @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
     private String id;
+    
+    @Column(unique = true)
     private String nis;
+    
+    @Column(nullable = false)
     private String nama;
-    private String tanggalLahir;
+    
+    @Column(name = "tanggal_lahir", nullable = false)
+    @Temporal(TemporalType.DATE)
+    private Date tanggalLahir;
+    
+    @Column(unique = true)
     private String jenisKelamin;
+    
+    @Column(nullable = false)
     private String alamat;
+    
+    @Column(nullable = false)
     private String agama;
+    
+    @Column(nullable = false)
     private String nilai;
     
 }
