bdf7924759b3b898242c1c5f626489e093cab044
alyaputerip29 <alyaputerip29@gmail.com>
2019-03-24 17:58:49 +0700

    aaksk

---
+++ aplikasi-pendaftaran/src/main/java/com/projek/prakerin/pendaftaran/entity/Siswa.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.projek.prakerin.pendaftaran.entity;
+
+/**
+ *
+ * @author HP
+ */
+public class Siswa {
+    
+}
