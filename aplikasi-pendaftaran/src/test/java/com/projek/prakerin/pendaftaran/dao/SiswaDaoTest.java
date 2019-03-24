2dd8da1ce0d532d95c17fb7ad9ba1a5ce7dc3856
alyaputerip29 <alyaputerip29@gmail.com>
2019-03-24 21:33:21 +0700

    bdhab

--- aplikasi-pendaftaran/src/test/java/com/projek/prakerin/pendaftaran/dao/SiswaDaoTest.java
+++ aplikasi-pendaftaran/src/test/java/com/projek/prakerin/pendaftaran/dao/SiswaDaoTest.java
@@ -9,18 +9,8 @@ import javax.sql.DataSource;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.jdbc.Sql;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
-@Sql(
-        executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD,
-        scripts ="/data/siswa.sql"
-)
 public class SiswaDaoTest {
     
     @Autowired
