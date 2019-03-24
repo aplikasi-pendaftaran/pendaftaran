04208a8262950d1df9af328ed294fc4e8923df4d
alyaputerip29 <alyaputerip29@gmail.com>
2019-03-25 01:05:17 +0700

    controllerhtml

--- aplikasi-pendaftaran/src/main/java/com/projek/prakerin/pendaftaran/controller/SiswaHtmlController.java
+++ aplikasi-pendaftaran/src/main/java/com/projek/prakerin/pendaftaran/controller/SiswaHtmlController.java
@@ -1,5 +1,22 @@
 package com.projek.prakerin.pendaftaran.controller;
 
+import com.projek.prakerin.pendaftaran.dao.SiswaDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping("/siswa")   
 public class SiswaHtmlController {
     
+    @Autowired
+    private SiswaDao sd;
+    
+    @RequestMapping("/list")
+    public void daftarSiswa(Model m){
+        m.addAtribute("daftarPeserta", sd.findAll());
+        
+    }
 }
