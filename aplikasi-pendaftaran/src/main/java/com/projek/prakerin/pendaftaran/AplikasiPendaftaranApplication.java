363ae1420ca4143303071d2acf4af66d9a33de07
alyaputerip29 <alyaputerip29@gmail.com>
2019-03-25 17:53:01 +0700

    layout

--- aplikasi-pendaftaran/src/main/java/com/projek/prakerin/pendaftaran/AplikasiPendaftaranApplication.java
+++ aplikasi-pendaftaran/src/main/java/com/projek/prakerin/pendaftaran/AplikasiPendaftaranApplication.java
@@ -2,6 +2,8 @@ package com.projek.prakerin.pendaftaran;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.thymeleaf.dialect.springdata.SpringDataDialect;
 
 @SpringBootApplication
 public class AplikasiPendaftaranApplication {
@@ -9,5 +11,10 @@ public class AplikasiPendaftaranApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AplikasiPendaftaranApplication.class, args);
 	}
+        
+        @Bean
+        public SpringDataDialect springDataDialect() {
+        return new SpringDataDialect();
+    }
 
 }
